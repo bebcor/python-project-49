6bde8b404123773e055dc6fb574ec97edfc7e9e1
иван шулубин <shulubin.ia@gmail.com>
2024-12-05 22:07:49 +0300

    Добавлена игра арифметическая прогрессия

---
+++ brain_games/progression.py
@@ -0,0 +1,46 @@
+import random
+import prompt
+
+
+def calc_the_progression():
+    random_first_element = random.randint(0, 100)
+    random_step = random.randint(1, 100)
+    random_missing_element = random.randint(0, 9)
+    progression_numbers = []
+
+    current_value = random_first_element
+
+    for i in range(10):
+        progression_numbers.append(current_value)
+        current_value += random_step
+
+    correct_answer = progression_numbers[random_missing_element]
+    progression_numbers[random_missing_element] = ".."
+
+    progression_str = " ".join(map(str, progression_numbers))
+    return progression_str, correct_answer
+
+def progression_game():
+    print("Welcome to the Brain Games!")
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}!")
+    print("What number is missing in the progression?")
+
+    counter = 0
+    while counter != 3:
+        progression_str, correct_answer = calc_the_progression()
+        print(progression_str)
+
+        user_answer = input("Your answer: ")
+
+        if user_answer.isdigit() and int(user_answer) == correct_answer:
+            print("Correct!")
+            counter += 1
+        else:
+            print(f"'{user_answer}' is a wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+            counter = 0
+
+    print(f"Congratulations, {name}!")
+
+
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from brain_games.progression import progression_games
+
+def main:
+    progression_games
+
+
+if __name__ == "__main__"
+    main()
